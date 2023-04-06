a1b9ba9edd721397bcf33d4d605950077433dddd
David Estephanian <desteph@protonmail.com>
2023-04-06 12:45:31 -0700

    Changed structure

--- sshgpg.sh
+++ sshgpg.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-set -e
-set -o pipefail
+set -euo pipefail
 
 # String formatters
 if [[ -t 1 ]]; then
@@ -13,19 +12,18 @@ fi
 tty_mkbold() { tty_escape "1;$1"; }
 tty_yellow="$(tty_escape "0;33")"
 tty_green="$(tty_mkbold 32)"
-tty_red="$(tty_mkbold 31)"
 tty_reset="$(tty_escape 0)"
 
-printf "\n${tty_yellow}====================Script starts====================${tty_reset}\n\n"
+printf "\n%s====================Script starts====================%s\n\n" "${tty_yellow}" "${tty_reset}"
 
 if [ ! -d ~/.ssh ] || [ ! -d ~/.gnupg ]; then
 	# https://medium.com/risan/upgrade-your-ssh-key-to-ed25519-c6e8d60d3c54
 	if [ ! -d ~/.ssh ]; then
-		printf "${tty_green}Creating SSH key pair for GitHub...${tty_reset}\n\n"
+		printf "%sCreating SSH key pair for GitHub...%s\n\n" "${tty_green}" "${tty_reset}"
 
-		read -p "${tty_green}Please enter the email you use for GitHub: ${tty_reset}" EMAIL
+		read -rp "${tty_green}Please enter the email you use for GitHub: ${tty_reset}" EMAIL
 
-		ssh-keygen -o -a 100 -t ed25519 -f ~/.ssh/id_ed25519 -C $EMAIL
+		ssh-keygen -o -a 100 -t ed25519 -f ~/.ssh/id_ed25519 -C "$EMAIL"
 		eval "$(ssh-agent -s)"
 
 		cat <<EOT >>~/.ssh/config
@@ -39,7 +37,7 @@ EOT
 	fi
 
 	if [ ! -d ~/.gnupg ]; then
-		printf "${tty_green}Creating GPG key pair for GitHub...${tty_reset}\n"
+		printf "%sCreating GPG key pair for GitHub...%s\n" "${tty_green}" "${tty_reset}"
 
 		gpg --full-generate-key
 
@@ -52,6 +50,6 @@ EOT
 	fi
 fi
 
-printf "${tty_green}Keys have been added to your system. Make sure to add them to GitHub${tty_reset}\n"
+printf "%sKeys have been added to your system. Make sure to add them to GitHub%s\n" "${tty_green}" "${tty_reset}"
 
-printf "\n${tty_yellow}====================Script ends====================${tty_reset}\n\n"
+printf "\n%s====================Script ends====================%s\n\n" "${tty_yellow}" "${tty_reset}"
