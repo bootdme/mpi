4d9c41af198b4c2b9f158a616043b3730a80ea6b
David E <desteph@protonmail.com>
2023-05-07 13:33:14 -0700

    Install distro

--- init.sh
+++ init.sh
@@ -5,6 +5,9 @@ set -euo pipefail
 # Rosetta
 softwareupdate --install-rosetta --agree-to-license
 
+# Install distro for dotbot
+pip3 install distro
+
 # Download dotfiles
 git clone https://github.com/bootdme/dotfiles.git
 cd dotfiles && ./install
